63b68525785179d5eb9430dca33749ef95d6b40f
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2022-12-31 17:07:01 -0500

    Update bt 2.py

--- bt 2.py
+++ bt 2.py
@@ -1,9 +1,13 @@
 from scratchclient import ScratchSession
+from sys import exit
 
 followerNumber = 0
 
 session = ScratchSession("BotTest0910", "Barbarsnarf")
 
-while 3 == 3:
-    print(session.get_user("SomeoneOnThelnternet").get_followers()[followerNumber].username)
-    followerNumber = followerNumber + 1
+while True:
+    try:
+        print(session.get_user("SomeoneOnThelnternet").get_followers()[followerNumber].username)
+        followerNumber = followerNumber + 1
+     except Exception as e:
+        exit(f"Error: {e}")
